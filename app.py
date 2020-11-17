007c7f1c7c541ee0a7bee8657a85531ae1706d15
mphil17 <mattphil48@gmail.com>
2020-11-17 14:36:30 +0000

    Linked addshow.html with database so that form is submitted.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,6 +12,8 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
@@ -21,6 +23,20 @@ def index():
 @app.route("/addshow")
 def addShow():
     return render_template("addshow.html")
+
+
+@app.route('/submitShow', methods=['POST'])
+def submitShow():
+    show = {
+        "name": request.form.get("name"),
+        "years": request.form.get("years"),
+        "seasons": request.form.get("seasons"),
+        "country": request.form.get("country"),
+        "synopsis": request.form.get("synopsis")
+    }
+    tvshow = mongo.db.show
+    tvshow.insert_one(show)
+    return redirect(url_for("addShow"))
 
 
 if __name__ == "__main__":
