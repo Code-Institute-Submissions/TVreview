52f21fe8b0937cd52d716ca08bca2d9e522e827e
mphil17 <mattphil48@gmail.com>
2020-12-23 18:28:26 +0000

    Added stars to tvshow.html. Created functionality for search bar.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,36 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems, {});
   });
+
+/*-------------IMDB API------------------*/  
+const baseURL = "https://imdb-api.com/API/SearchSeries/k_y8kd93un/"
+  
+function getData(search, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + search);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+        
+            cb(data = JSON.parse(this.responseText));
+        
+        }
+    };
+}
+
+function writeToDocument(search) {
+    getData(search, function (data){
+        /*clear results*/
+        var clear = document.getElementById('data');
+        clear.innerHTML = "";
+        data = data.results;
+        data.forEach(function(searchResult) {
+        document.getElementById('data').innerHTML += "<p>" + searchResult.title + "</p>";
+        });
+        
+    })
+}
